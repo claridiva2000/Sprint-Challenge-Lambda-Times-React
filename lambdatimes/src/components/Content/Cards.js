e58bfd78bd901306eefbfbafdf62befdd3152951
claridiva2000 <claridiva2000@gmail.com>
2019-03-23 14:39:26 -0500

    maps fixed. tab click function is active.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,21 +4,17 @@ import Card from './Card';
 import "../../CSS/index.css"
 const Cards = props => {
   return (
-    <div className="cards-container">
-    {props.cards.map( card =>(
-      <div className="card" key={card.headline}>
-        <div className="headline">{card.headline}</div>
-      <p>{card.tab}</p>
-      
-      <div className="author"><span>{card.author}</span><div className="img-container"><img src={card.img} alt="img"/></div></div>
-      </div>
-    ))}
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
-}
+    <div className="cards-container" >
+      {props.cards.map( c =><Card key={c.headline} card={c}/>)}
 
+    </div>
+
+  );
+};
+
+
+// {/* Using the cards prop, map over the list creating a 
+//           new Card component for each passing the card as the only prop*/}
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -3,16 +3,18 @@ import Tab from './Tab';
 
 import "../../CSS/index.css"
 
-const Tabs = props => {
+
+
+const Tabs = (props) => {
+
+ 
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-            {props.tabs.map( tab =>(
-              <div key={tab} className="tab">{tab}</div>
-            ))}
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          
+          {props.tabs.map(t =><Tab key={t} tab={t}/>)}
+      
       </div>
     </div>
   );
@@ -20,3 +22,5 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+  {/* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
